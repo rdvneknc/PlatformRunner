6d9f9a757534c6f636b8ada32aa08ddb8f5e81ae
Rıdvan Ekinci <ridvanekinci92@gmail.com>
2022-08-14 17:43:49 +0300

    update

--- Assets/Scripts/Play With Opponents/OpponentController.cs
+++ Assets/Scripts/Play With Opponents/OpponentController.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class OpponentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform targetPosition;
+    public NavMeshAgent navMeshAgent;
+
+    private void Awake()
     {
-        
+        GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        navMeshAgent.destination = targetPosition.position;
     }
 }
